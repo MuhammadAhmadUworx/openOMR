7d90f56f70c8655a8db5c73f933490b25b7459c0
Muhammad Ahmad <ahmadsheikh617@gmail.com>
2026-02-26 00:44:16 +0500

    initializing BE spring-boot project

---
+++ src/omr_processor/src/main/java/io/github/baeyung/omr_processor/OmrProcessorApplication.java
@@ -0,0 +1,12 @@
+package io.github.baeyung.omr_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmrProcessorApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(OmrProcessorApplication.class, args);
+    }
+}
---
+++ src/omr_processor/src/test/java/io/github/baeyung/omr_processor/OmrProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.baeyung.omr_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmrProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
